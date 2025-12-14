866e2d45a7bf4fae513fac2ec4b2b46663957dc0
yugalsharmaandtc <yugalsharmaandtc@gmail.com>
2025-12-14 16:25:29 +0530

    7 try

--- backend/db/init.sql
+++ backend/db/init.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS users (
     name VARCHAR(50) NOT NULL,
     email VARCHAR(100) UNIQUE NOT NULL,
     phone VARCHAR(15),
-    password VARCHAR(255) NOT NULL,
+    password_hash VARCHAR(255) NOT NULL,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
